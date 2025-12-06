e772a69716a8848f06d13e44d66a47616cea34b9
Archit Garg <architga@usc.edu>
2025-12-06 02:00:20 +0000

    Implement wordle and schedwork solutions

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>& shifts_used,
+    size_t position,
+    size_t day
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +46,82 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t num_of_days = avail.size();
+    size_t num_of_workers = avail[0].size();
+
+    sched.assign(num_of_days,std::vector<Worker_T>(dailyNeed,INVALID_ID));
+    std::vector<size_t> shifts_used(num_of_workers,0);
+
+    bool answer = schedule_helper(avail,dailyNeed,maxShifts,sched,shifts_used,0,0);
+
+    return answer; 
+}
 
 
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>& shifts_used,
+    size_t position,
+    size_t day
+)
+{
 
+
+  size_t num_of_days = avail.size();
+  size_t num_of_workers = avail[0].size();
+
+  if(day == num_of_days)
+  {
+    return true;
+  }
+
+  if(position == dailyNeed)
+  {
+    return schedule_helper(avail,dailyNeed,maxShifts,sched,shifts_used,0,day+1);
+  }
+for(size_t w = 0;w<num_of_workers;++w)
+  {
+    if(!avail[day][w])
+    {
+      continue;
+    }
+
+    if(shifts_used[w] >= maxShifts)
+    {
+      continue;
+    }
+
+    bool previously_used = false; 
+
+    for(size_t i = 0;i<position;++i)
+    {
+      if(sched[day][i] == w)
+      {
+      previously_used = true; 
+      break;
+      } 
+    }
+
+    if(previously_used)
+      {
+        continue;
+      }
+ 
+
+    sched[day][position] = w; 
+    shifts_used[w]++;
+
+    if(schedule_helper(avail,dailyNeed,maxShifts,sched,shifts_used,position+1,day))
+    {
+      return true;
+    }
+
+    sched[day][position] = INVALID_ID;
+    shifts_used[w]--;
+    }
+  return false; 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,9 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <string>
+#include <vector>
+
 #endif
 
 #include "wordle.h"
@@ -13,7 +16,16 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+std::string remove_single_character(const std::string& floating_values_string, char c);
+void wordle_helper(
+  int i,
+  std::string& cur_partial_word,
+  const std::set<std::string>& valid_words,
+  const std::string& arrangement,
+  const std::string& remaining_floating,
+  std::set<std::string>& results,
+  const std::vector<int>& num_blanks_left
+);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,6 +35,118 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::set<std::string> results; 
+
+    int input_size = static_cast<int>(in.size());
+    
+    if(input_size ==0)
+    {
+      return results; 
+    }
+    
+
+    std::vector<int> blanks_remaining(input_size+1);
+
+    blanks_remaining[input_size] = 0;
+
+    for(int i = input_size-1 ;i>=0; --i)
+    {
+      if(in[i] == '-')
+      {
+        blanks_remaining[i] = blanks_remaining[i+1] +1;
+      }
+      else
+      {
+        blanks_remaining[i] = blanks_remaining[i+1];
+      }
+    }
+    std::string cur_partial_word = in;
+    wordle_helper(0,cur_partial_word,dict,in,floating,results,blanks_remaining);
+    return results; 
 }
 
 // Define any helper functions here
+
+
+std::string remove_single_character(const std::string& floating_values_string, char c)
+{
+  std::string result;
+  bool removed = false;
+
+  for(size_t i = 0; i < floating_values_string.size(); i++)
+  {
+    if(!removed && floating_values_string[i] == c)
+    {
+      removed = true; 
+      continue;
+    }
+      result += floating_values_string[i];
+  }
+  return result;
+}
+
+
+void wordle_helper(
+  int i,
+  std::string& cur_partial_word,
+  const std::set<std::string>& valid_words,
+  const std::string& arrangement,
+  const std::string& remaining_floating,
+  std::set<std::string>& results,
+  const std::vector<int>& num_blanks_left
+)
+{
+  int cur_size = static_cast<int>(cur_partial_word.size());
+
+  if(i == cur_size)
+  {
+    if(remaining_floating.empty() && valid_words.find(cur_partial_word) != valid_words.end())
+    {
+      results.insert(cur_partial_word);
+    }
+    return;
+  }
+
+  int num_blank_left = num_blanks_left[i];
+  int num_required = static_cast<int>(remaining_floating.size());
+  if(num_required > num_blank_left)
+  {
+    return;
+  }
+
+  if(arrangement[i]!= '-')
+  {
+    cur_partial_word[i] = arrangement[i];
+    wordle_helper(i+1,cur_partial_word,valid_words,arrangement,remaining_floating,results,num_blanks_left);
+    return;
+  }
+
+  if(num_required == num_blank_left)
+  {
+    for(size_t j =0; j< remaining_floating.size();j++)
+    {
+      char c = remaining_floating[j];
+      cur_partial_word[i] = c;
+      std::string new_remaining_floating = remove_single_character(remaining_floating,c);
+      wordle_helper(i+1,cur_partial_word,valid_words,arrangement,new_remaining_floating,results,num_blanks_left);
+
+    }
+  }
+  
+  else
+  {
+    for(size_t j = 0;j< remaining_floating.size();++j)
+    {
+      char c =remaining_floating[j];
+      cur_partial_word[i] = c;
+      std::string new_remaining_floating = remove_single_character(remaining_floating,c);
+      wordle_helper(i+1,cur_partial_word,valid_words,arrangement,new_remaining_floating,results,num_blanks_left);
+    }
+
+    for(char car = 'a'; car <='z'; ++car)
+    {
+      cur_partial_word[i] = car; 
+      wordle_helper(i+1,cur_partial_word,valid_words,arrangement,remaining_floating,results,num_blanks_left);
+    }
+  }
+}
